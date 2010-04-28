565d6c3b3961c5e8f6896c4de257724c3972186d
Chris Dawes <chris@aupeo.com>
2010-04-28 08:46:19 +0200

    add bot_state table, add beanId to plugins table. move data to separate file.

---
+++ db-data.sql
@@ -0,0 +1,92 @@
+-- MySQL dump 10.11
+--
+-- Host: localhost    Database: irc
+-- ------------------------------------------------------
+-- Server version	5.0.83-0ubuntu3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `aliases`
+--
+
+LOCK TABLES `aliases` WRITE;
+/*!40000 ALTER TABLE `aliases` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aliases` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `bot_state`
+--
+
+LOCK TABLES `bot_state` WRITE;
+/*!40000 ALTER TABLE `bot_state` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bot_state` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `errors`
+--
+
+LOCK TABLES `errors` WRITE;
+/*!40000 ALTER TABLE `errors` DISABLE KEYS */;
+INSERT INTO `errors` VALUES ('memusage','memusage: %0','no'),('version','version: 0.1.1','no');
+/*!40000 ALTER TABLE `errors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `idents`
+--
+
+LOCK TABLES `idents` WRITE;
+/*!40000 ALTER TABLE `idents` DISABLE KEYS */;
+/*!40000 ALTER TABLE `idents` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `plugins`
+--
+
+LOCK TABLES `plugins` WRITE;
+/*!40000 ALTER TABLE `plugins` DISABLE KEYS */;
+INSERT INTO `plugins` VALUES ('!version','elsie.plugins.Version',NULL);
+/*!40000 ALTER TABLE `plugins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `transcript`
+--
+
+LOCK TABLES `transcript` WRITE;
+/*!40000 ALTER TABLE `transcript` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transcript` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2010-04-28  8:44:41
--- db.sql
+++ db.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.37, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.11
 --
 -- Host: localhost    Database: irc
 -- ------------------------------------------------------
--- Server version	5.1.37-1ubuntu5.1
+-- Server version	5.0.83-0ubuntu3
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,19 +23,22 @@ DROP TABLE IF EXISTS `aliases`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `aliases` (
-  `Nick` varchar(20) DEFAULT NULL,
-  `Alias` varchar(20) DEFAULT NULL
+  `Nick` varchar(20) default NULL,
+  `Alias` varchar(20) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `aliases`
+-- Table structure for table `bot_state`
 --
 
-LOCK TABLES `aliases` WRITE;
-/*!40000 ALTER TABLE `aliases` DISABLE KEYS */;
-/*!40000 ALTER TABLE `aliases` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `bot_state`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bot_state` (
+  `angry` tinyint(1) default '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `errors`
@@ -45,21 +48,11 @@ DROP TABLE IF EXISTS `errors`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `errors` (
-  `name` varchar(100) DEFAULT NULL,
-  `message` varchar(255) DEFAULT NULL,
-  `Public` char(4) DEFAULT NULL
+  `name` varchar(100) default NULL,
+  `message` varchar(255) default NULL,
+  `Public` char(4) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `errors`
---
-
-LOCK TABLES `errors` WRITE;
-/*!40000 ALTER TABLE `errors` DISABLE KEYS */;
-INSERT INTO `errors` VALUES ('memusage','memusage: %0','no'),('version','version: 0.1.1','no');
-/*!40000 ALTER TABLE `errors` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `idents`
@@ -69,20 +62,11 @@ DROP TABLE IF EXISTS `idents`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `idents` (
-  `Ident` varchar(255) DEFAULT NULL,
-  `User` varchar(20) DEFAULT NULL,
-  `Unique` char(40) DEFAULT NULL
+  `Ident` varchar(255) default NULL,
+  `User` varchar(20) default NULL,
+  `Unique` char(40) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `idents`
---
-
-LOCK TABLES `idents` WRITE;
-/*!40000 ALTER TABLE `idents` DISABLE KEYS */;
-/*!40000 ALTER TABLE `idents` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `plugins`
@@ -92,21 +76,12 @@ DROP TABLE IF EXISTS `plugins`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `plugins` (
-  `command` varchar(20) DEFAULT NULL,
-  `classname` varchar(255) DEFAULT NULL,
+  `command` varchar(20) default NULL,
+  `classname` varchar(255) default NULL,
+  `beanId` varchar(50) default NULL,
   UNIQUE KEY `plugins_command_index` (`command`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `plugins`
---
-
-LOCK TABLES `plugins` WRITE;
-/*!40000 ALTER TABLE `plugins` DISABLE KEYS */;
-INSERT INTO `plugins` VALUES ('!version','elsie.plugins.Version');
-/*!40000 ALTER TABLE `plugins` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `transcript`
@@ -116,23 +91,14 @@ DROP TABLE IF EXISTS `transcript`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `transcript` (
-  `channel` varchar(20) DEFAULT NULL,
-  `DateTime` datetime DEFAULT NULL,
-  `command` varchar(20) DEFAULT NULL,
-  `prefix_nick` varchar(20) DEFAULT NULL,
-  `ident` varchar(255) DEFAULT NULL,
-  `escaped_params` varchar(255) DEFAULT NULL
+  `channel` varchar(20) default NULL,
+  `DateTime` datetime default NULL,
+  `command` varchar(20) default NULL,
+  `prefix_nick` varchar(20) default NULL,
+  `ident` varchar(255) default NULL,
+  `escaped_params` varchar(255) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `transcript`
---
-
-LOCK TABLES `transcript` WRITE;
-/*!40000 ALTER TABLE `transcript` DISABLE KEYS */;
-/*!40000 ALTER TABLE `transcript` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -142,22 +108,13 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `Username` varchar(20) DEFAULT NULL,
-  `Op` char(4) DEFAULT NULL,
-  `Voice` char(4) DEFAULT NULL,
-  `Enforce` char(4) DEFAULT NULL,
-  `Annoy` char(4) DEFAULT NULL
+  `Username` varchar(20) default NULL,
+  `Op` char(4) default NULL,
+  `Voice` char(4) default NULL,
+  `Enforce` char(4) default NULL,
+  `Annoy` char(4) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -168,4 +125,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-04-23 16:45:40
+-- Dump completed on 2010-04-28  8:45:38
